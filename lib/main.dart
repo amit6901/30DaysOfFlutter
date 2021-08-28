e652e78492abe848192b7c1a32b04ec30aeb7087
Amit Shradrao Kandalkar <amit.kandalkar06@gmail.com>
2021-08-28 16:56:18 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to 30 days of flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
